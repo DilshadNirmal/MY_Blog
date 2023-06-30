6591d4705635a0a4f934050239f56a1711d4b6b6
DilshadNirmal <dilshadgov2020@gmail.com>
2023-06-30 19:51:09 +0530

    minuscle changes

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,8 +6,8 @@ import "../styles/global.css";
 export default function Nav() {
     const [toggle, setToggle] = useState(false)
     return (
-        <nav>
-            <svg width={250} height={4} viewBox="0 0 250 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <nav className="relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium">
+            <svg className="absolute bottom-0 left-1/2 -translate-x-1/2" width={250} height={4} viewBox="0 0 250 4" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2 2L428 2" 
                 strokeWidth={2}
                 stroke="#282828" 
@@ -18,13 +18,13 @@ export default function Nav() {
             </div>
 
             {/* {title} */}
-            <h1><a href="/">Hua.</a></h1>
-            <h1><a href="/about">About</a></h1>
+            <h1 className="text-1g font-bold"><a href="/">Hua.</a></h1>
+            <h1 className="text-1g font-bold"><a href="/about">About</a></h1>
 
-            <div className="hamburger">
-                <span className="line"></span>
-                <span className="line"></span>
-                <span className="line"></span>
+            <div className="space-y-1">
+                <span className="block h-0.5 w-8 bg-black"></span>
+                <span className="block h-0.5 w-6 bg-black"></span>
+                <span className="block h-0.5 w-4 bg-black"></span>
             </div>
         </nav>
     )
